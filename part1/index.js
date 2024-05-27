3a65bb0797d2d2e220935c797454e2b8fc145db8
Nitin-web-dev <nitinwebdev5@gmail.com>
2024-05-27 16:13:48 +0530

    addead part 1 with navbar example and basic gsap

--- part1/index.js
+++ part1/index.js
@@ -1,18 +1,93 @@
-gsap.to('#box', {
-    x: 1000,
-    duration: 3,
-    delay: 1.5,
-    rotate: 180,
-    scale: 2,
-    backgroundColor: "blue",
-    borderRadius: "30%",
+// gsap.to('#box', {
+//     x: 1000,
+//     duration: 3,
+//     delay: 1.5,
+//     rotate: 180,
+//     scale: 2,
+//     backgroundColor: "blue",
+//     borderRadius: "30%",
+// })
+
+// gsap.from('#box2', {
+//     x: 1000,
+//     duration: 3,
+//     delay: 1.5,
+//     rotate: 180,
+//     scale: 2,
+
+// })
+
+
+// another exmaple of h1
+
+// gsap.from('h1', {
+//         duration:1,
+//         delay: 1,
+//         y: 20,
+//         opacity: 0,
+//         stagger: 0.3,
+        
+// })
+
+
+// repeat exmaple
+// gsap.to('.box', {
+//     x: 1000,
+//     duration: 3,
+//     delay: 1,
+//     rotate: 180,
+//     scale: 2,
+//     backgroundColor: "blue",
+//     borderRadius: "30%",
+//     repeat: 1,
+//     yoyo: true,
+// })
+
+
+
+
+// timeline example
+
+// let tl = gsap.timeline();
+
+// tl.to("#box1", {
+//     x: 1000,
+//     duration: 3,
+//     delay: 1
+    
+// })
+// tl.to("#box2", {
+//     x: 1000,
+//     duration: 3,
+
+// })
+// tl.to("#box3", {
+//     x: 1000,
+//     duration: 3,
+
+// })
+
+
+
+
+
+// navbar exmaple
+let tl = gsap.timeline();
+
+tl.from('h2', {
+    y:20,
+    delay:1,
+    opacity: 0,
 })
-
-gsap.from('#box2', {
-    x: 1000,
-    duration: 3,
-    delay: 1.5,
-    rotate: 180,
-    scale: 2,
-
+tl.from('h4',{
+    y:20,
+    opacity: 0,
+    stagger: 0.3
+})
+tl.from('h1', {
+    y:20,
+    scale:0.5,
+    duration: 0.5,
+    
+    opacity: 0,
 })
