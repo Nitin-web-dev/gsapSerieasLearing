90fcd5ffb84bea781942261ab0892984c435d752
Nitin-web-dev <nitinwebdev5@gmail.com>
2024-05-27 15:33:57 +0530

    addead basic gsap

---
+++ part1/index.js
@@ -0,0 +1,18 @@
+gsap.to('#box', {
+    x: 1000,
+    duration: 3,
+    delay: 1.5,
+    rotate: 180,
+    scale: 2,
+    backgroundColor: "blue",
+    borderRadius: "30%",
+})
+
+gsap.from('#box2', {
+    x: 1000,
+    duration: 3,
+    delay: 1.5,
+    rotate: 180,
+    scale: 2,
+
+})
